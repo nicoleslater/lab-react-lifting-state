9758e74586a0975d4846327c5741e048e00ac468
Nicole Slater <nicoleslater@pursuit.org>
2023-08-02 21:16:06 -0400

    Setting things up

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,10 @@
 export default function Footer() {
   return;
+  <footer>
+          <ul>
+            <li>Contact</li>
+            <li>About</li>
+            <li>Legal</li>
+          </ul>
+        </footer>
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,11 @@
 export default function Header() {
-  return;
+  return; 
+  (
+  <div> 
+  <header>
+  <h1 className="color-change-5x">RSVP App</h1>
+</header>
+
+</div>
+  )
 }
